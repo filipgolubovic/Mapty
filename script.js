7a3aaa76862b4af72e25f89f2eb49e93a58006f6
filipgolubovic <golubovic.filip8@gmail.com>
2023-01-02 21:22:35 +0100

    App design

---
+++ script.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const months = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'Jun',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
+
+const form = document.querySelector('.form');
+const containerWorkouts = document.querySelector('.workouts');
+const inputType = document.querySelector('.form__input--type');
+const inputDistance = document.querySelector('.form__input--distance');
+const inputDuration = document.querySelector('.form__input--duration');
+const inputCadence = document.querySelector('.form__input--cadence');
+const inputElevation = document.querySelector('.form__input--elevation');
+
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      console.log(position);
+    },
+    function () {
+      alert('Ne mozemo pronaci Vasu lokaciju!');
+    }
+  );
+}
